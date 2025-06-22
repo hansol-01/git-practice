9e3392b4bd76355836f19a871ab64bb96a74717e
hansol-01 <gksthgus2022@gmail.com>
2025-06-22 20:30:29 +0900

    아이디어톤_분실물_JPA

---
+++ LikeLionCatchit/src/main/java/com/example/likelioncatchit/LikeLionCatchitApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelioncatchit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLionCatchitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeLionCatchitApplication.class, args);
+	}
+
+}
---
+++ LikeLionCatchit/src/test/java/com/example/likelioncatchit/LikeLionCatchitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelioncatchit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLionCatchitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
